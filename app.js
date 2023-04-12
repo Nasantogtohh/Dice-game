b2cd864c6ef971ad4c5e9daa0204ef672901808f
Nasantogtohh <nasantogtokhbayanmunkh@gmail.com>
2023-04-12 16:31:36 +0800

    progrom ehlehed beldsen huwisagch uusgesen

---
+++ app.js
@@ -0,0 +1,18 @@
+// Toglogchin eeljig hadgalah huwisagch,1-r toglogc 0, 2-r toglogcyg 1 gey
+var activePlayer = 1;
+//Toglogchin tsugluulsan onoog hadgalah huwisagch
+var scores = [0, 0];
+//Toglogchin eeljindee tsugluulj baigaa onoog hadgalah huwisagch
+var roundScore = 0;
+//Shooni ali talaaraa buusnig hadgalah huwisagch, 1-6 gesen utgyg sanamsarguigeer uusgej ogno
+// var dice = Math.floor(Math.random() * 6) + 1;
+
+//Program ehlehed beltgey
+document.querySelector("#score-0").textContent = 0;
+//textcontent iin orond innerhtml bicwel html code bicej bolno//
+document.querySelector("#score-1").textContent = 0;
+document.querySelector(".dice").style.display = "none";
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+
+//
